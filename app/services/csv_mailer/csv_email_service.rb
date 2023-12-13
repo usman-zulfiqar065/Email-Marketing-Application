4699e1c18774e4ce5c09a367d0e349b76bd46460
usman-zulfiqar065 <usman.zulfiqar065@gmail.com>
2023-12-13 10:45:29 -0800

    Created home controller with approprite actions

--- app/services/csv_mailer/csv_email_service.rb
+++ app/services/csv_mailer/csv_email_service.rb
@@ -1,8 +1,12 @@
 module CsvMailer
   class CsvEmailService
 
-    def perform
-      UserMailer.send_email('chmusman1999@gmail.com', 'subject: testing').deliver_later
+    def perform(options)
+      @company_name = options[:company_name]
+      @business_email = options[:business_email]
+      @csv = options[:csv_file]
+      
+      # UserMailer.send_email('chmusman1999@gmail.com', 'subject: testing').deliver_later
     end
 
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  post '/create_emails', to: 'home#create_emails', as: 'create_emails'
 end
