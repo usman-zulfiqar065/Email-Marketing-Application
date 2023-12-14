5666646e8b0bc79cce64c7cf27a6ed15da82309b
usman-zulfiqar065 <usman.zulfiqar065@gmail.com>
2023-12-13 20:45:27 -0800

    Added CSV parser functionality

--- app/services/csv_mailer/csv_email_service.rb
+++ app/services/csv_mailer/csv_email_service.rb
@@ -1,10 +1,20 @@
 module CsvMailer
   class CsvEmailService
+    require 'csv'
 
-    def perform(options)
-      @company_name = options[:company_name]
-      @business_email = options[:business_email]
-      @csv = options[:csv_file]
+    def perform(params)
+      @company_name = params[:company_name]
+      @business_email = params[:business_email]
+      @csv = params[:csv_file]
+
+      file_path = File.open(@csv)
+
+      CSV.foreach(file_path, headers: true) do |row|
+        # debugger.binding
+
+        puts row['Email']
+        puts row['Name']
+      end
       
       # UserMailer.send_email('chmusman1999@gmail.com', 'subject: testing').deliver_later
     end
