bff4a579db110f38b51d2a3fe7a0b48c3e744d6a
usman-zulfiqar065 <usman.zulfiqar065@gmail.com>
2023-12-28 04:36:50 -0800

    Generated user scaffold with required fields

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
 
   resources :leads
   resources :businesses
+  resources :users, except: %i[new create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  post '/create_emails', to: 'home#create_emails', as: 'create_emails'
 end
