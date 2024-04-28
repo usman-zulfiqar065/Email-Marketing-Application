595b6bc2cda5ef16903d2032ae54f5653f79f9d4
usman-zulfiqar065 <usman.zulfiqar065@gmail.com>
2024-04-28 04:54:20 -0700

    Active admin configuration

--- Gemfile
+++ Gemfile
@@ -78,3 +78,5 @@ gem 'devise', '~> 4.9'
 gem 'sidekiq', '~> 7.2'
 
 gem 'activeadmin'
+
+gem 'sassc-rails'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,17 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def authenticate_admin_user!
+    raise SecurityError unless current_user.admin?
+  end
+
+  rescue_from SecurityError do
+    flash[:alert] = 'You are not authorized for this action'
+    redirect_to root_path
+  end
+
+  private
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name password])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[name password])
