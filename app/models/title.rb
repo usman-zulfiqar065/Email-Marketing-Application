70c4e849c6d527cf3278486081eb242718c14f40
usman-zulfiqar065 <usman.zulfiqar065@gmail.com>
2024-04-28 05:09:08 -0700

    Created country and title resources

--- app/models/title.rb
+++ app/models/title.rb
@@ -2,4 +2,8 @@ class Title < ApplicationRecord
   validates :name, presence: true
 
   has_many :compaigns
+
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[id name created_at]
+  end
 end
